fd0ed19c6791cc371cf5a1cdbe4d909a2657f19c
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-05-30 17:57:12 +0530

    update

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -20,7 +20,7 @@ console.log(students.batch);
 
 // 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
 
-students.isAdult = `true/false`;
+students.isAdult = true;
 
 // 7. Using `console.log` print the value of `isAdult` key from the `students` object
 
@@ -39,6 +39,8 @@ console.log(students[42]);
 objectLength = Object.keys(students).length;
 console.log(objectLength);
 
+// console.log(students.length);
+
 // 11. Can you define a key of `let or var` in any object? Reason.
 
 // yes as key is a string so 'let' is not same as let keyword.
@@ -50,5 +52,5 @@ console.log(students);
 
 // 13. Update the value of the key batch in the object `students` and print it using `console.log`
 
-objectLength = Object.keys(students).length;
-console.log(objectLength);
+students.batch = 10;
+console.log(students.batch);
