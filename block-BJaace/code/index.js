16f5aab995a2d177450046f30ee369a0bc9fda09
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-05-31 09:45:57 +0530

    update

--- block-BJaace/code/index.js
+++ block-BJaace/code/index.js
@@ -1,20 +1,41 @@
 // 1. Declare an empty array and store it in a variable.
 
+example = [];
+
 // 2. Now check the type of empty array you declared above.
+
+console.log(typeof(example));
 
 // 3. Create an array named `colors` that contains five different names of colors as strings.
 
+colors = ["red", "blue", "green", "maroon", "violet"];
+
 // 4. Access the first color in the array and print it to the console using `console.log()`
+
+console.log(colors[0]);
 
 // 5. Access the third color in the array and print it to the console using `console.log()`
 
+console.log(colors[2]);
+
 // 6. Access the last color in the array and print it to the console using `console.log()`
+
+lastValue = colors.length;
+console.log();
 
 // 7. Write one line of code that changes the value of the first color in the list to "black" (overwriting the previous value).
 
+colors[0] = "black";
+
 // 8. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
 
+console.log(colors);
+colors.push = "ultraviolet";
+console.log(colors);
+
 // 9. Now, Find the length of the array named `colors`.
+
+console.log(colors.length);
 
 // 10. Create a new variable called fourthColor and set it equal to the fourth color in the list.
 
